a1d8a9e4bd42a25fe0ae11907982180b2f681b50
mel <mcmelon@nodiumhosting.com>
2025-09-07 08:07:38 +0200

    chore: project setup

---
+++ src/main/java/ing/boykiss/skynarchy/Skynarchy.java
@@ -0,0 +1,11 @@
+package ing.boykiss.skynarchy;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Skynarchy implements ModInitializer {
+    public static final String MOD_ID = "skynarchy";
+
+    @Override
+    public void onInitialize() {
+    }
+}
